d4d0a053c7285832771aa80c37d66ce48d862d43
ozygdmr <uozdemir1981@gmail.com>
2025-05-06 03:35:09 +0300

    Rigidbody

--- Assets/MoveForward.cs
+++ Assets/MoveForward.cs
@@ -3,19 +3,53 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    GameObject capsule;
+   
     void Start()
     {
-        capsule = GameObject.Find("Capsule");
+        
+        myrigid = this.GetComponent<Rigidbody>();
+        myrigid.maxLinearVelocity = angular;
+        
     }
-    public int speed = 300;
-    
+    public int speed;
+    public Rigidbody myrigid;
+    public float playerheight;
+    private RaycastHit hit;
+    public float angular;
+    public float damp;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) {
-            transform.position += capsule.transform.forward * speed * Time.deltaTime;
+        Physics.Raycast(this.transform.position, Vector3.down, out hit,2f*0.5f+0.2f);
+        if (Input.GetKey(KeyCode.W) && hit.collider.CompareTag("Ground")) {
+            myrigid.linearVelocity += transform.forward * speed * Time.deltaTime;
+            
+        }
+        if (Input.GetKey(KeyCode.Space)&& hit.collider.CompareTag("Ground"))
+        {
+            myrigid.AddForce(Vector3.up,ForceMode.Impulse);
+
+        }
+        if (Input.GetKey(KeyCode.A) && hit.collider.CompareTag("Ground"))
+        {
+            
+            myrigid.linearVelocity -= transform.right  * speed * Time.deltaTime;
+            
+        }
+        if (Input.GetKey(KeyCode.S) && hit.collider.CompareTag("Ground"))
+        {
+            myrigid.linearVelocity -= transform.forward * speed * Time.deltaTime;
+            
+        }
+        if (Input.GetKey(KeyCode.D) && hit.collider.CompareTag("Ground"))
+        {
+            myrigid.linearVelocity += transform.right * speed * Time.deltaTime;
+            
         }
         
+        if(hit.collider.CompareTag("Ground"))
+        {
+            myrigid.linearDamping = damp;
+        }
     }
 }
